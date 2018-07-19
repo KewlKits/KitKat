a0e76405d20a795707b4209324919c8e9badfdaf
Natalie Ghidali <nghidali@fb.com>
2018-07-19 10:30:33 -0700

    Added View Controller placeholders for the pool and the queue

---
+++ KitKat/Pool/PoolCell.m
@@ -0,0 +1,24 @@
+//
+//  PoolCell.m
+//  KitKat
+//
+//  Created by Natalie Ghidali on 7/19/18.
+//  Copyright © 2018 kewlkits. All rights reserved.
+//
+
+#import "PoolCell.h"
+
+@implementation PoolCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ KitKat/Queue/QueueCell.m
@@ -0,0 +1,24 @@
+//
+//  QueueCell.m
+//  KitKat
+//
+//  Created by Natalie Ghidali on 7/19/18.
+//  Copyright © 2018 kewlkits. All rights reserved.
+//
+
+#import "QueueCell.h"
+
+@implementation QueueCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
