f7a12c696f5ed151a33b23bbae49dca37883245e
Miles Olson <mpolson64@gmail.com>
2018-07-30 15:48:57 -0700

    Info in detail view

--- KitKat/Entry/Controllers/PartyDetailViewController.m
+++ KitKat/Entry/Controllers/PartyDetailViewController.m
@@ -6,10 +6,16 @@
 //  Copyright © 2018 kewlkits. All rights reserved.
 //
 
+#import <CoreLocation/CoreLocation.h>
 #import "PartyDetailViewController.h"
 #import "BackendAPIManager.h"
+#import "SpotifyDataManager.h"
 
 @interface PartyDetailViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ownerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic) IBOutlet UILabel *counterLabel;
 
 @end
 
@@ -18,6 +24,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.nameLabel.text = self.party.name;
+    
+    [[BackendAPIManager shared] getAUser:self.party.ownerId withCompletion:^(UNIHTTPJsonResponse *response, NSError *error) {
+        if (!error) {
+            User *owner = [[User alloc] initWithDictionary:response.body.object];
+            
+            dispatch_async(dispatch_get_main_queue(), ^(void) {
+                self.ownerLabel.text = [NSString stringWithFormat:@"Host: %@", owner.name];
+            });
+        }
+    }];
+    
+    CLGeocoder *geocoder = [CLGeocoder new];
+    CLLocation *partyLocation = [[CLLocation alloc] initWithLatitude:[self.party.location[1] doubleValue] longitude:[self.party.location[0] doubleValue]];
+    
+    [geocoder reverseGeocodeLocation:partyLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        if(!error && placemarks && placemarks.count > 0) {
+            self.addressLabel.text = [NSString stringWithFormat:@"%@", placemarks[0].name];
+        }
+        else {
+            self.addressLabel.text = @"Unknown location";
+        }
+    }];
+    
+    self.counterLabel.text = [NSString stringWithFormat:@"%lu in pool • %lu in queue", self.party.pool.count, self.party.queue.count];
 }
 
 - (void)didReceiveMemoryWarning {
