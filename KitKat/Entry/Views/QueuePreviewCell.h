8bd3d26424e2be1cc3a036d1f36fe57710b29d27
Miles Olson <mpolson64@gmail.com>
2018-07-30 16:32:20 -0700

    Added queue tableview to detail view

---
+++ KitKat/Entry/Views/QueuePreviewCell.h
@@ -0,0 +1,13 @@
+//
+//  QueuePreviewCell.h
+//  KitKat
+//
+//  Created by Miles Olson on 7/30/18.
+//  Copyright © 2018 kewlkits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QueuePreviewCell : UITableViewCell
+
+@end
